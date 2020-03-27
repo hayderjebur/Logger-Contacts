edf62a1a3b6c889934cd484f4635bfb21dbb3d63
Hayder Jebur <hayderjebur@Hayders-MacBook-Air.local>
2020-03-27 08:50:07 -0700

    add home and about pages

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,19 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export const Navbar = ({ title, icon }) => {
   return (
     <div className="navbar bg-primary">
       <h1>
-        <i className={icon} />
-        {title}
+        <i className={icon} /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
